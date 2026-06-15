381b81c4044c984a655980a9ed20d7deb65f00bc
MauseSchüler <gomme@gmx.ch>
2026-06-15 15:10:59 +0200

    test fix

--- M158/M158-Backend/app/db_base.py
+++ M158/M158-Backend/app/db_base.py
@@ -5,7 +5,7 @@ db_host = os.environ['DB_HOST']
 db_user = os.environ['DB_USER']
 db_password = os.environ['DB_PASSWORD']
 db_name = os.environ['DB_NAME']
-db_port = os.environ['DB_PORT']
+db_port = int(os.environ['DB_PORT'])
 
 
 CONN_PARAMS = {
